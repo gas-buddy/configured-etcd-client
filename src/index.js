eb5576fb45165f11f2c40c6605dda118c4a36f09
Max Metral <max@pyralis.com>
2016-11-25 15:23:23 -0500

    Allow empty hosts arg and use defaults, and pass options separately

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import assert from 'assert';
 import Etcd from 'node-etcd';
 
 export default class EtcdClient {
   constructor(context, opts) {
-    assert(opts, 'configured-etcd-client must be passed arguments');
-    assert(opts.hosts, 'configured-etcd-client missing hosts setting');
-
+    const { hosts, options } = (opts || {});
     if (context && context.logger && context.logger.info) {
       context.logger.info('Initializing etcd client', {
-        hosts: opts.hosts,
+        hosts: hosts || '<default>',
       });
     }
-    this.etcd = new Etcd(opts.hosts);
+    this.etcd = new Etcd(hosts, options);
   }
 
   async start() {
--- tests/test_connection.js
+++ tests/test_connection.js
@@ -3,9 +3,7 @@ import winston from 'winston';
 import EtcdClient from '../src/index';
 
 tap.test('test_connection', async (t) => {
-  const etcd = new EtcdClient({ logger: winston }, {
-    hosts: ['etcd'],
-  });
+  const etcd = new EtcdClient({ logger: winston });
   const client = await etcd.start();
   t.ok(client.get, 'Should have a get method');
   t.ok(client.set, 'Should have a set method');
