adbc28d0509b50767ef0c7522bd912e57509a114
Matthew Pavlinsky <mpavlinsky@gmail.com>
2017-09-20 13:51:18 -0400

    Move common etcd methods to configured-etcd-client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import { EventEmitter } from 'events';
 import Etcd from 'node-etcd';
+import os from 'os';
+import Lock, { AlreadyLockedError } from 'microlock';
 
-export default class EtcdClient {
+function statusCode(error) {
+  if (error) {
+    return error.errorCode || 'unknown';
+  }
+  return 0;
+}
+
+export default class EtcdClient extends EventEmitter {
   constructor(context, opts) {
+    super();
     const { hosts, options } = (opts || {});
     if (context && context.logger && context.logger.info) {
       context.logger.info('Initializing etcd client', {
@@ -12,6 +23,105 @@ export default class EtcdClient {
   }
 
   async start() {
-    return this.etcd;
+    return this;
+  }
+
+  finishCall(callInfo, status) {
+    this.emit('finish', { status, ...callInfo });
+  }
+
+  async get(context, key) {
+    const callInfo = {
+      client: this,
+      context,
+      key,
+      method: 'get',
+    };
+    this.emit('start', callInfo);
+    return new Promise((accept, reject) => {
+      this.etcd.get(key, (error, value) => {
+        this.finishCall(callInfo, statusCode(error));
+        if (error && error.errorCode === 100) {
+          accept();
+        } else if (error) {
+          reject(error);
+        }
+        accept(value ? value.node.value : null);
+      });
+    });
+  }
+
+  /**
+   * ttl is in seconds
+   */
+  async set(context, key, value, ttl) {
+    const callInfo = {
+      client: this,
+      context,
+      key,
+      value,
+      ttl,
+      method: 'set',
+    };
+    this.emit('start', callInfo);
+    return new Promise((accept, reject) => {
+      this.etcd.set(key, value, { ttl }, (error) => {
+        this.finishCall(callInfo, statusCode(error));
+        if (error) {
+          reject(error);
+        } else {
+          accept();
+        }
+      });
+    });
+  }
+
+  parseValue(v) {
+    if (v && v[0] === '[') {
+      try {
+        return JSON.parse(v);
+      } catch (error) {
+        // Do nothing, fall through
+      }
+    }
+    return [];
+  }
+
+  async acquireLock(context, key) {
+    const callInfo = {
+      client: this,
+      context,
+      key,
+      method: 'acquireLock',
+    };
+    this.emit('start', callInfo);
+    const lock = new Lock(this.etcd, key, os.hostname(), 10);
+    let alerted = false;
+    lock.once('unlock', () => {
+      alerted = true;
+    });
+    for (let attempt = 0; attempt < 5; attempt += 1) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        await lock.lock();
+        context.gb.logger.info('Acquired lock', { key });
+        this.finishCall(callInfo, 'acq');
+        return lock;
+      } catch (error) {
+        context.gb.logger.warn('Lock contention', { key, attempt });
+        if (!(error instanceof AlreadyLockedError)) {
+          this.finishCall(callInfo, 'err');
+          throw error;
+        }
+        // eslint-disable-next-line no-await-in-loop
+        await Promise.delay(250 * attempt);
+        if (alerted) {
+          this.finishCall(callInfo, 'wait-acq');
+          return lock;
+        }
+      }
+    }
+    this.finishCall(callInfo, 'timeout');
+    throw new Error('Timed out waiting for lock');
   }
 }
---
+++ tests/test_event.js
@@ -0,0 +1,39 @@
+import tap from 'tap';
+import winston from 'winston';
+import EtcdClient from '../src/index';
+
+tap.test('test_get_event', async (t) => {
+  const context = { logger: winston };
+  const etcd = new EtcdClient(context);
+  const client = await etcd.start();
+  const keyName = 'testkey';
+
+  client.addListener('start', (info) => {
+    t.equal(info.key, keyName);
+    t.equal(info.method, 'get');
+  });
+  client.addListener('finish', (info) => {
+    t.equal(info.key, keyName);
+    t.equal(info.method, 'get');
+  });
+
+  await client.get(context, keyName);
+});
+
+tap.test('test_set_event', async (t) => {
+  const context = { logger: winston };
+  const etcd = new EtcdClient(context);
+  const client = await etcd.start();
+  const keyName = 'testkey';
+
+  client.addListener('start', (info) => {
+    t.equal(info.key, keyName);
+    t.equal(info.method, 'set');
+  });
+  client.addListener('finish', (info) => {
+    t.equal(info.key, keyName);
+    t.equal(info.method, 'set');
+  });
+
+  await client.set(context, keyName, 0, 60);
+});
